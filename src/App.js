1ca71268683bdceaaa36a5fc4ee592677e56ab96
Martin Zachrison <marzac@nordnet.se>
2018-04-25 10:41:40 +0200

    Added dependency react-jss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Page, Header, Content, Footer } from './components';
 import React, { Component } from 'react';
+import { Page, Header, Content, Footer } from './components';
+import { Grid, Row, Col } from '@nordnet/grid';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
