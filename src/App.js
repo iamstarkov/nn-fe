e7702330ec802a5bf6902db00ee0ff6d6a875c1d
Martin Zachrison <marzac@nordnet.se>
2018-04-25 15:50:05 +0200

    Sticky footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
-import {Content, Header, Footer, Page} from './components';
+import {Content, Footer, Header, Page} from './components';
 
 class App extends Component {
     render() {
         return (
             <Page>
-                <Header/>
-                <Content/>
+                <div>
+                    <Header/>
+                    <Content/>
+                </div>
                 <Footer/>
             </Page>
         );
