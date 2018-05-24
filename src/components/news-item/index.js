bda8427220936d41785a13ba5f29c3a00fdafa58
Martin Zachrison <marzac@nordnet.se>
2018-05-24 16:09:37 +0200

    Async with logs

--- src/components/news-item/index.js
+++ src/components/news-item/index.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {distanceInWordsToNow} from 'date-fns';
 import {api} from '../../utils';
 import "./style.css"
 
 
-export const NewsItem = ({id}) => {
-    const item = api.getItem(id)
-    const timeInMs = item.time * 1000;
-    const age = distanceInWordsToNow(new Date(timeInMs));
-    const comments = (item.descendants === 0) ? "discuss" : item.descendants + " comments";
+export class NewsItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {item: {}}
+    }
 
-    return (
-        <div>
-            <div className={"news_item"}>
-                <a href={item.url} className={"title"}>{item.title}</a>
-                <br/>
-                <Link to={`/item/${item.id}`}>{item.score}</Link>
-                {' by '}
-                <Link to={`/item/${item.id}`}>{item.by}</Link>
-                {' '}
-                <Link to={`/item/${item.id}`}>{age}</Link>
-                {'  | '}
-                <Link to={`/item/${item.id}`}>{comments}</Link>
+    componentDidMount() {
+        api.getItem(this.props.id).then(item => this.setState({item}));
+    }
+
+    render() {
+        const item = this.state.item
+        const timeInMs = item.time * 1000;
+        const age = distanceInWordsToNow(new Date(timeInMs));
+        const comments = (item.descendants === 0) ? "discuss" : item.descendants + " comments";
+        return (
+            <div>
+                <div className={"news_item"}>
+                    <a href={item.url} className={"title"}>{item.title}</a>
+                    <br/>
+                    <Link to={`/item/${item.id}`}>{item.score}</Link>
+                    {' by '}
+                    <Link to={`/item/${item.id}`}>{item.by}</Link>
+                    {' '}
+                    <Link to={`/item/${item.id}`}>{age}</Link>
+                    {'  | '}
+                    <Link to={`/item/${item.id}`}>{comments}</Link>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
--- src/components/page-news-list/index.js
+++ src/components/page-news-list/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {api} from '../../utils';
 import {NewsItemList} from "../";
 import './style.css';
 
-export const PageNewsList = () => (
-        <NewsItemList ids={api.getItemsIds()}/>
-);
+
+const isArraysEqual = (arr1 = [], arr2 = []) => arr1.toString() === arr2.toString();
+
+export class PageNewsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ids: undefined,
+        };
+        this.fetchIds = () => {
+            api.getItemsIds()
+                .then(ids => {this.setState({ids})})
+                .catch(err => console.error(err));
+        }
+    }
+
+    componentDidMount() {
+        console.log("mounted", this);
+        this.fetchIds();
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        const shouldUpdate = !isArraysEqual(this.state.ids, nextState.ids);
+        console.log('should update', shouldUpdate);
+        return shouldUpdate
+    }
+
+    render() {
+        const { ids } = this.state;
+        console.log('render data:', ids);
+        if (!ids) {
+            return <div>Loading…</div>
+        }
+        return (
+            <div>
+                <NewsItemList ids={ids}/>
+                <button onClick={this.fetchIds}>Refresh</button>
+            </div>
+        );
+    }
+}
