a53efe2555c815809b8ea12a78e9c51accab215b
Martin Zachrison <marzac@nordnet.se>
2018-05-30 13:09:39 +0200

    WIP redux

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,28 +1,7 @@
-// `by`—The username of the item's author.
-// `descendants`—In the case of stories or polls, the total comment count.
-// `id`—The item's unique id.
-// `score`—The story's score, or the votes for a pollopt.
-// `time`—Creation date of the item, in Unix Time (seconds).
-// `title`—The title of the story, poll or job.
-// `type`—The type of item. One of "job", "story", "comment", "poll", or "pollopt".
-// `url`—The URL of the story.
-
-
+const BASE_URL = 'https://hacker-news.firebaseio.com';
 const fetchJson = url => fetch(url).then(res => res.json());
-const first10 = arr => arr.slice(0, 10);
-
-// Items Ids Endpoint
-const getItemsIds = () => {
-    console.log('fetching...');
-    return fetchJson('https://hacker-news.firebaseio.com/v0/topstories.json').then(res => first10(res));
-};
-
-// Item endpoint https://hacker-news.firebaseio.com/v0/item/8863.json
-const getItem = (id) => {
-    return fetchJson(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-};
 
 export const api = {
-    getItemsIds,
-    getItem,
+    getItem: id => fetchJson(`${BASE_URL}/v0/item/${id}.json`),
+    getItemsIds: () => fetchJson(`${BASE_URL}/v0/topstories.json`),
 };
